486d7a2c123159e6ab76c6f9b250d2601c2aa7fd
Ricardo Alvarado <ricardoalvaradoaponte@gmail.com>
2018-12-02 13:06:30 -0500

    Primer Commit - rest server

---
+++ index.ts
@@ -0,0 +1,24 @@
+import Server from './clases/server';
+import router from './routes/router';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const server = new Server();
+
+// Middleware para la configuracion del bodyparser
+server.app.use( bodyParser.urlencoded({ extended: true }));
+server.app.use( bodyParser.json() );
+
+// COrs
+
+server.app.use(cors({
+    origin: true,
+    credentials: true
+}));
+
+// Middleware para las rutas
+server.app.use('/', router);
+
+server.start(() => {
+    console.log(`El Servidor esta corriendo en el puerto: ${server.port}`);
+});
